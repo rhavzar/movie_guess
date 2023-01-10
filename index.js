bc91de4d5132f04e46ea8003988eccb3efccbfba
rhavzar <johnpaulsherrill@gmail.com>
2023-01-09 22:35:51 -0600

    final commit pre-db

--- index.js
+++ index.js
@@ -25,8 +25,6 @@ let guesses = 1;
 let filmNum = Math.floor(Math.random() * 15 + 1);
 
 
-
-document.getElementById("header").innerHTML=(film_list[filmNum - 1]);
 document.getElementById("frame").src=(filePrefix + "" + filmNum + "" + fileMidfix + "1" + filePostfix);
 
 function buttonFunctionality(button_name)
@@ -37,21 +35,72 @@ function buttonFunctionality(button_name)
 function submitButton()
 {
     guesses = guesses + 1;
-    if (guesses >= 7)
-        {
-            // add end game functionality
-            document.getElementById("guess-count").innerHTML=("Popup Needed");
-            exit();
-        }
     let guess = document.getElementById('textbox').value;
     if (guess == film_list[filmNum - 1])
     {
-        document.getElementById("guess-count").innerHTML=("Popup Needed");
+        gameEndW();
         exit();
+    }
+    if (guesses >= 7)
+    {
+            // add end game functionality
+            gameEndL();
+            exit();
     }
 
     document.getElementById('frame_select').innerHTML=(document.getElementById('frame_select').innerHTML + "\n<button onclick=\"buttonFunctionality(" + guesses + ");\">" + guesses + "</button>")
     document.getElementById("frame").src=(filePrefix + "" + filmNum + "" + fileMidfix + guesses + filePostfix);
     document.getElementById('textbox').value="";
-    document.getElementById("guess-count").innerHTML=((7 - guesses) + " guesses remain");
+    if (7 - guesses == 1)
+        document.getElementById("guess-count").innerHTML=((7 - guesses) + " guess remains");
+    else
+        document.getElementById("guess-count").innerHTML=((7 - guesses) + " guesses remain");
+}
+
+function gameEndL() {
+    document.getElementById("pregame").innerHTML=("");
+    document.getElementById("endgame").innerHTML=(""
+        + "\t<p id=\"endgame-m\">" + film_list[filmNum - 1] + "</p>\n"
+        + "\t<div class=\"compbox\">\n"
+        + "\t\t<span class=\"gb\" id=\"red\"></span>\n"
+        + "\t\t<span class=\"gb\" id=\"red\"></span>\n"
+        + "\t\t<span class=\"gb\" id=\"red\"></span>\n"
+        + "\t\t<span class=\"gb\" id=\"red\"></span>\n"
+        + "\t\t<span class=\"gb\" id=\"red\"></span>\n"
+        + "\t\t<span class=\"gb\" id=\"red\"></span>\n"
+        + "\t</div>\n"
+        + "\t<div class=\"play-again\" onclick=\"window.location.reload(true);\">Play Again</div>\n"
+        +  "\t<div class=\"save\"><a href=\"#\">Save Stats</a></div>\n"
+    );
+}
+
+function gameEndW() {
+    for (let i = guesses; i < 7; i++)
+    {
+        document.getElementById('frame_select').innerHTML=(document.getElementById('frame_select').innerHTML + "\n<button onclick=\"buttonFunctionality(" + i + ");\">" + i + "</button>")
+    }
+    document.getElementById("frame").src=(filePrefix + "" + filmNum + "" + fileMidfix + 6 + filePostfix);
+    document.getElementById("pregame").innerHTML=("");
+
+    let buttons = "";
+    let j = 0;
+    for (let i = guesses; i > 2; i--)
+    {
+        buttons += "\t\t<span class=\"gb\" id=\"red\"></span>\n";
+        j++;
+    }
+    buttons += "\t\t<span class=\"gb\" id=\"green\"></span>\n";
+    for ( ; j < 5; j++)
+    {
+        buttons += "\t\t<span class=\"gb\"></span>\n";
+    }
+
+    document.getElementById("endgame").innerHTML=(""
+    + "\t<p id=\"endgame-m\">Got It!</p>\n"
+    + "\t<div class=\"compbox\">\n"
+    + buttons
+    + "\t</div>\n"
+    + "\t<div class=\"play-again\" onclick=\"window.location.reload(true);\">Play Again</div>\n"
+    +  "\t<div class=\"save\"><a href=\"#\">Save Stats</a></div>\n");
+    console.log(document.getElementById("endgame").innerHTML);
 }
